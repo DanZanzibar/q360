81c8324956c7d9fa525fff641fa00a24905e5a6d
Zan Owsley <zanowsley@gmail.com>
2023-02-24 15:37:32 -0700

    removed extra files

--- q360/sort-customers-2023.py
+++
@@ -1,9 +0,0 @@
-import os
-os.chdir("C:\\Users\\zanow\\OneDrive\\Desktop\\Scripts\\Q360")
-import work_module as wm
-
-os.chdir("C:\\Users\\zanow\\OneDrive\\Desktop\\Scripts\\Q360\\2023")
-
-wm.format_raw_customer_file('raw-outside-customers', 'outside-customers-no-bookings')
-wm.format_raw_bookings_file('raw-bookings', 'bookings')
-wm.add_bookings_data('outside-customers-no-bookings', 'bookings', 'outside-customers')
